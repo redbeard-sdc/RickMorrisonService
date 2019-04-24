97593399e085fd84e92d9c3aa768c8b6c8d170ec
Dan Carbonell <dan1525@gmail.com>
2019-04-24 17:31:31 -0600

    final FB seeding scripts, using PG as db of choice

--- server/db_FB.js
+++ server/db_FB.js
@@ -1,18 +1,21 @@
-const admin = require('firebase-admin');
+// const admin = require('firebase-admin');
 
-const serviceAccount = require('./archive/sdc-hotel-pricing-238420-7e59d551eda0.json');
+// const serviceAccount = require('./archive/sdc-hotel-pricing-238420-7e59d551eda0.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://sdc-hotel-pricing.firebaseio.com',
-});
-
-const db_FB = admin.firestore();
-
-// db_FB.collection('pricing').get().then((querySnapshot) => {
-//   console.log(querySnapshot.size);
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://sdc-hotel-pricing.firebaseio.com',
 // });
 
-console.log('hi');
+// const db_FB = admin.firestore();
 
-module.exports = db_FB;
+// db_FB.collection('pricing').get().then((querySnapshot) => {
+//   console.log('From db_FB: ', querySnapshot.size);
+// });
+
+// const pricingRef = db_FB.collection('pricing');
+
+// const query = pricingRef.where('Agoda', '==', '170');
+// console.log(query);
+
+// module.exports = db_FB;
--- server/server.js
+++ server/server.js
@@ -9,19 +9,18 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 
+// ***** Connection to PostgreSQL database *****
+
+const db_PG = require('./queries_PG');
+
+app.get('/prices/:day', db_PG.getPricesByDay);
+
 
 // ***** Connection to Google Cloud Firestore database *****
 
-const db_FB = require('./queries_FB');
+// const db_FB = require('./queries_FB');
 
-app.get('/prices/:day', db_FB.getPricesByDay);
-
-
-// ***** Connection to PostgreSQL database *****
-
-// const db_PG = require('./queries_PG');
-
-// app.get('/prices/:day', db_PG.getPricesByDay);
+// app.get('/prices/:day', db_FB.getPricesByDay);
 
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}...`));
